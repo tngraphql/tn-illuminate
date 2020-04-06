5b10d21119239135eee0eb35d9beb6cdf083f83e
nguyenpl117 <nguyenpl117@gmail.com>
2020-04-06 11:20:00 +0700

    v1.1.23-alpha

---
+++ src/Contracts/database/index.ts
@@ -0,0 +1,13 @@
+/**
+ * (c) Phan Trung Nguyên <nguyenpl117@gmail.com>
+ * User: nguyenpl117
+ * Date: 4/6/2020
+ * Time: 11:15 AM
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export class index {
+
+}
